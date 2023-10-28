a88a596f8eebf7c98f305ed61e5d8920608c18a6
Robert Kearns <rxk0914@hotmail.com>
2023-10-28 12:41:11 -0600

    Tasks stubbed out

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 import argparse
 
+import requests
+from bs4 import BeautifulSoup as Soup
+
+
+def google_search(query: str):
+    resp = requests.get(
+        'https://www.google.com/search',
+        params={
+            'q': query,
+            'og': query
+        }
+    )
+    if resp.status_code != 200:
+        print(f'Google returned {resp.status_code}')
+        return
+
+    parser = Soup(resp.content)
+    link = parser.select_one('a[href^="/url"]')
+    if not link:
+        print(f'Failed to locate link for {query}')
+        return
+
+    resp = requests.get('https://www.google.com' + link.get('href'))
+    if resp.status_code != 200:
+        print(f'Google returned {resp.status_code}')
+        return
+
+    parser = Soup(resp.content)
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-
+    parser.add_argument('-s', '--site', default='stackoverflow', type=str)
     parser.add_argument('prompt', type=str, nargs='+')
 
     args = parser.parse_args()
     args.prompt = ' '.join(args.prompt)
 
     return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    print(args.prompt)
---
+++ stub.py
@@ -0,0 +1,37 @@
+import dataclasses
+from typing import Optional
+
+
+@dataclasses.dataclass
+class Question:
+    question_id: int
+    title: str
+    short_description: Optional[str]
+
+
+@dataclasses.dataclass
+class QuestionAnswer:
+    username: str
+    user_points: int
+    up_votes: int
+    body_html: str
+
+
+def get_question_answers(question: Question) -> list[QuestionAnswer]:
+    # TODO: Josh
+    ...
+
+
+def get_search_results(term: str, site='stackoverflow.com') -> list[Question]:
+    # TODO: Robert
+    ...
+
+
+def display_question_list(questions: list[Question]):
+    # TODO: Mitul
+    ...
+
+
+def display_answers(question: Question, answers: list[QuestionAnswer]):
+    # TODO: Mitul
+    ...
